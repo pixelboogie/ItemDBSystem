90c066ef631561491c4bbad1ebdfacf7c53c1c66
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-24 10:51:27 -0700

    Add files via upload

---
+++ ItemDB.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+    public List<Item> itemDatabase = new List<Item>();
+
+    public void AddItem(int itemID, MyPlayer player)
+    {
+        // check if item matches something in our db
+        foreach(var item in itemDatabase)
+        {
+            // check for match
+            if(item.id == itemID)
+            {
+                Debug.Log("Add item");
+                // check for available inventory slots would be added here
+                player.inventory[0] = item;
+                return;
+            }
+        }
+    }
+
+    public void RemoveItem(int itemID, MyPlayer player)
+    {
+         foreach(var item in itemDatabase)
+         {
+             if(item.id == itemID)
+             {
+                 player.inventory[0] = null;
+                 Debug.Log("Remove item");
+             }
+         }
+    }
+}
+
+
---
+++ MyPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayer : MonoBehaviour
+{
+    public Item[] inventory;
+
+    private ItemDB _itemDatabase;
+
+
+
+    private void Start()
+    {
+        _itemDatabase = GameObject.Find("ItemDB").GetComponent<ItemDB>();
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // request item by ID
+            _itemDatabase.AddItem(0, this);
+
+        }
+        else if(Input.GetKeyDown(KeyCode.R))
+        {
+            // remove item by ID
+            _itemDatabase.RemoveItem(0, this);
+
+        }
+    }
+}
